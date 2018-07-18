900817b45401c21456cc37a696133b4b284940c0
Milly R <millz118@gmail.com>
2018-07-18 14:17:33 -0400

    commit

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ class Pokemon {
     }
 }
 
-
 // define trainer class
 class Trainer {
     constructor(name) {
@@ -18,10 +17,11 @@ class Trainer {
         this.myPokemonList = {}
     }
     add(pokemonID) {
-        // axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`).then((poke) => {
-        axios.get(`https://pokeapi-nycda.firebaseio.com/pokemon/${pokemonID}.json`).then((poke) => {
+        // return axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonID}/`).then((poke) => {
+        return axios.get(`https://pokeapi-nycda.firebaseio.com/pokemon/${pokemonID}.json`).then((poke) => {
 
             let pokedata = poke.data;
+            console.log(pokedata);
             let name = pokedata.name;
             let hp = pokedata.stats[5].base_stat
             let attack = pokedata.stats[4].base_stat;
@@ -32,13 +32,11 @@ class Trainer {
             console.log(`${pokemonID} added sucessfully`);
             console.log(newPoke)
             this.myPokemonList[newPoke.name] = newPoke
-            
-        })  
+
+        });
     }
     get(pokemon) {
-        // returns a Pokemon object housing information for the pokemon it found
-        // console.log(pokemon);
-        return this.myPokemonList[pokemon]
+        return this.myPokemonList[pokemon];
     }
     all() {
         // no parameters/returns an array of Pokemon objects
@@ -48,79 +46,37 @@ class Trainer {
 }
 
 // create new instance of trainer & class here
+// will also be able to add any other pokemon by number
 let milly = new Trainer("milly");
-milly.add(5);
-milly.add(37);
-milly.add(79);
 
-// // API CALL
-// axios.get('https://pokeapi.co/api/v2/pokemon/5/').then((poke) => {
-//         // console.log(poke);
-//         let pokedata  = poke.data;
-//         // console.log(pokedata);
-//         let name = pokedata.name;
-//         console.log(name)
-//         let hp = pokedata.stats[5].base_stat
-//         console.log(hp);
-//         let attack = pokedata.stats[4].base_stat;
-//         console.log(attack);
-//         let defense = pokedata.stats[3].base_stat;
-//         console.log(defense);
-//         let abilities = pokedata.abilities.map(   (element) => element.ability.name);
-//         console.log(abilities);
-//         let sprite = pokedata.sprites.front_default;
-//         console.log(sprite);
 
-//         let charmeleon = new Pokemon(name,hp,attack,defense,abilities,sprite)
-//         milly.add(charmeleon);
-//         pokeCounter.push(charmeleon);
+// Grab the template script
+var theTemplateScript = $("#pokemon-template").html();
+// Compile the template
+var theTemplate = Handlebars.compile(theTemplateScript);
 
-// })
 
-// axios.get('https://pokeapi.co/api/v2/pokemon/37/').then((poke) => {
-//         // console.log(poke);
-//         let pokedata  = poke.data;
-//         // console.log(pokedata);
-//         let name = pokedata.name;
-//         console.log(name)
-//         let hp = pokedata.stats[5].base_stat
-//         console.log(hp);
-//         let attack = pokedata.stats[4].base_stat;
-//         console.log(attack);
-//         let defense = pokedata.stats[3].base_stat;
-//         console.log(defense);
-//         let abilities = pokedata.abilities.map(   (element) => element.ability.name);
-//         console.log(abilities);
-//         let sprite = pokedata.sprites.front_default;
-//         console.log(sprite);
+Promise.all([
+  milly.add(5),
+  milly.add(37),
+  milly.add(79)
+]).then(() => {
+  renderList(milly); 
+});
 
-//         let vulpix = new Pokemon(name,hp,attack,defense,abilities,sprite)
-//         milly.add(vulpix);
-//         pokeCounter.push(vulpix);
+function renderList(trainer){
+  let pokeList = trainer.myPokemonList;
+  let context = {
+    content: pokeList
+  };
 
-// })
+  // Pass our data to the template
+  const theCompiledHtml = theTemplate(context);
 
-// axios.get('https://pokeapi.co/api/v2/pokemon/79/').then((poke) => {
-//         // console.log(poke);
-//         let pokedata  = poke.data;
-//         // console.log(pokedata);
-//         let name = pokedata.name;
-//         console.log(name)
-//         let hp = pokedata.stats[5].base_stat
-//         console.log(hp);
-//         let attack = pokedata.stats[4].base_stat;
-//         console.log(attack);
-//         let defense = pokedata.stats[3].base_stat;
-//         console.log(defense);
-//         let abilities = pokedata.abilities.map(   (element) => element.ability.name);
-//         console.log(abilities);
-//         let sprite = pokedata.sprites.front_default;
-//         console.log(sprite);
-
-//         let slowpoke = new Pokemon(name,hp,attack,defense,abilities,sprite)
-//         milly.add(slowpoke);
-//         pokeCounter.push(slowpoke);
-
-// });
-
-// console.log(pokeCounter);
+  // Add the compiled html to the page
+  // Why use jQuery here? DOMElement.append('htmlString') doesn't transform the html string 
+  // into dom elements $jqueryElement.append('htmlString') does
+  let container = $('#pokemon-container')
+  console.log(theCompiledHtml)
+  container.append(theCompiledHtml);
+}
